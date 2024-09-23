7aa59f068b8b83b670bd188103a7d8a01fc6b5ed
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-09-23 23:52:00 +0400

    header,main,footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='app-footer'>
+      <div>
+        <b> ჩვენ შესახებ</b>
+        <div className='footer-info'> მოსაყოლი ბევირა ... </div>
+        
+      </div>
+      <div>
+      <b> პროდუქტები</b>
+      <ul>
+        <li>ყავა</li>
+        <li>და</li>
+        <li>ლუდი</li>
+      </ul>
+      </div>
+      <div>
+      <b> კონტაქტი</b>
+      <div className='contact-info'>
+        <div>phone: +995 ... </div>
+        <div>e-mail: mail@mail.ge</div>
+         
+      </div>
+
+      </div>
+      
+    </div>
   )
 }
--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import './Header.css';
+import logo from './logo.svg';
 
 export default function Header() {
   return (
-    <div>Header</div>
+    <div className='app-header'>
+      
+      
+      <img src={logo} alt="Logo" />
+      <b>მთავარი</b>
+    </div>
   )
 }
 
