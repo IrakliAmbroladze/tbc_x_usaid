249c1d9d34544505708d624c4fd8653668ce7f46
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-09-27 04:27:26 +0400

    create grio component

---
+++ src/components/item_grid/ItemGrid.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ItemGrid.css'
+
+
+export function ItemGrid() {
+  return (
+    <div className='item_grid'>
+      <div className='column1'>column 1</div>
+      <div className='column2'>column 2</div>
+      <div className='column3'>column 3</div>
+      <div className='column4'>column 4</div>
+      <div className='column5'>column 5</div>
+      <div className='column6'>column 6</div>
+    </div>
+  )
+}
