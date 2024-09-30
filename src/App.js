a64a655330de4d9b0d42b4afa69d0ee7da340b36
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-09-30 21:14:40 +0400

    add Contact and About page components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import Header from './components/header/Header';
 import Main from './components/main/Main';
 import Footer from './components/footer/Footer';
@@ -6,11 +8,26 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <Main/>
-      <Footer/>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+        <Footer/>
+      </Router>
     </div>
   );
+}
+
+function Contact(){
+  return <h2 style={{color: "red"}}>Contact page</h2>
+}
+
+function About (){
+  return <h2>About page</h2>
+
 }
 
 export default App;
