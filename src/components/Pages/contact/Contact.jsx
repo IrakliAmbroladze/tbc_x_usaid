f78c2e0d368d0a39570f532e886b9d89085603ac
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-09-30 22:20:29 +0400

    added css and user form

---
+++ src/components/Pages/contact/Contact.jsx
@@ -0,0 +1,53 @@
+import './Contact.css'
+
+
+export default function Contact(){
+  return (
+    <main className='contact-main'>
+      <b>Contact page</b>
+      <div className='contactInfo'>
+        <ul>
+          <dd>
+            <b>ტესტ კომპანია</b>
+          </dd>
+          <dd>
+            მისამართი: ნუცუბიძის ქუჩა
+          </dd>
+
+          <dd>
+          phone: +995 ...
+          </dd>
+          <dd>
+          e-mail: e-mail@e-mail.ge
+
+          </dd>
+          <dd>
+            დაგვიტოთ საკონტაქტო:
+          </dd>
+          <dd>
+
+           სახლი <input></input>
+          </dd>
+          <dd>
+           ტელეფონი <input></input>
+
+          </dd>
+
+          <dd>
+
+           მეილი <input></input>
+          </dd>
+          
+          <dd>
+
+           <button> submit </button>
+          </dd>
+
+        </ul>
+      
+      
+      </div>
+    </main>
+
+  )
+}
