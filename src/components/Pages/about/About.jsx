7a78da7ff2523bb1efe15fcca3bef46ee26201b5
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-09-30 22:19:36 +0400

    added css and some draft text

---
+++ src/components/Pages/about/About.jsx
@@ -0,0 +1,12 @@
+import './About.css'
+
+export default function About (){
+  return (
+    <main className='about-main'>
+      <b>About page</b>
+      <section>
+        some company selling some tables. თუ კონკრეტულად ამ პროექტის აღწერაზეა საუბარი, მეც არ ვიცი ეს საიტი რაზეა. ჩემი შავად სამუშაო სივრცეა ამ ეტაპზე
+      </section>
+    </main>
+  )
+}
