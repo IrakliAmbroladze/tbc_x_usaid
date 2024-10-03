bd7a40ba02b8c5de2f97453626769ee4888c1ede
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-03 23:21:37 +0400

    added key and alt to products

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 const ProductCart =(props) => {
   const {id, name, image, description} = props.data;
   return (
-    <div>
-      <img src={image} style={{width: 150, height: 150}}></img>
+    <div key={id}>
+      <img src={image} alt={name} style={{width: 150, height: 150}}></img>
       <h4>{name}</h4>
       <div>{description}</div>
       <div>
