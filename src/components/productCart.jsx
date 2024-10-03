50f7d14a5b8a29cc136ab9234f1a6532e64920b7
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-03 23:43:12 +0400

    css corrections on item-grid

--- src/components/productCart.jsx
+++ src/components/productCart.jsx
@@ -4,14 +4,13 @@ const ProductCart =(props) => {
   const {id, name, image, description} = props.data;
   return (
     <div key={id}>
-      <img src={image} alt={name} style={{width: 150, height: 150}}></img>
+      <img src={image} alt={name} style={{width: '50%', height: '50%'}}></img>
       <h4>{name}</h4>
       <div>{description}</div>
       <div>
-      <button>
-        Add to cart
-      </button>
-
+        <button>
+          Add to cart
+        </button>
       </div>
     </div>
   )
