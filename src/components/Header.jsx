311b9b99b340f78631ca4f7b9b3bafc557838a96
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-04 00:30:19 +0400

    add BLOD in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,6 +7,7 @@ export default function Header() {
         <NavLink to="/" exact>Home</NavLink> | 
         <NavLink to="/assignement-3">Assignment 3</NavLink> | 
         <NavLink to="/profile">Profile</NavLink> | 
+        <NavLink to="/blog">BLOG</NavLink> | 
         <NavLink to="/contact">Contact</NavLink> | 
         <NavLink to="/about">About</NavLink>
       </nav>
