5059d2d97a645bbacc8ac5a6c91f6f5af8be5a41
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-04 00:16:40 +0400

    add profile in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ export default function Header() {
       <nav>
         <NavLink to="/" exact>Home</NavLink> | 
         <NavLink to="/assignement-3">Assignment 3</NavLink> | 
+        <NavLink to="/profile">Profile</NavLink> | 
         <NavLink to="/contact">Contact</NavLink> | 
         <NavLink to="/about">About</NavLink>
       </nav>
