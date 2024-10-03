0b7a32106111dfb7928210e8a857b82dd4fb3ebb
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-04 01:55:06 +0400

    create user profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,27 @@
+import avatar from '../assets/images/avatar.jpg';
+
 export default function Profile (){
    return(
-    <div> hello form profile</div>
+    <div className="container">
+      
+        <h1>User profile</h1>
+        <img 
+          className="avatar"
+          src={avatar} 
+          alt="user_photo" 
+          width={100}
+          height={100}
+          ></img>
+        <div>
+          <b>სახელი: </b>
+          <input placeholder='შეიყვანე სახელი'></input> 
+          <br></br>
+          <b>გვარი: </b>
+          <input placeholder='შეიყვანე გვარი'></input>
+          <br></br>
+          <b>მეილი: </b>
+          <input placeholder='შეიყვანე მეილი'></input>
+        </div>
+    </div>
    )
 }
