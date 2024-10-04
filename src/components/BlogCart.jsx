f6091f8d4061c08ca21b129d6a308b842b9dd357
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-04 10:34:47 +0400

    Create BlogCart

---
+++ src/components/BlogCart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import BtnReadMore from './small/BtnReadMore';
+
+const BlogCart =(props) => {
+  const {id, name, image, description} = props.data;
+  return (
+    <div key={id} className='cartClass'>
+      <img src={image} alt={name} style={{width: '50%', height: '50%'}}></img>
+      <h4>{name}</h4>
+      <div>{description}</div>
+      <BtnReadMore />
+    </div>
+  )
+}
+
+export default BlogCart
