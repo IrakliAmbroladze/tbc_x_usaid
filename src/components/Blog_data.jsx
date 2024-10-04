1d131518c0bddd4a45b3f425880a8d325bdbd46a
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-04 09:53:05 +0400

    create blog data array

---
+++ src/components/Blog_data.jsx
@@ -0,0 +1,45 @@
+import blogImage1 from '../assets/images/blogImage1.jpg'
+import blogImage2 from '../assets/images/blogImage2.jpg'
+import blogImage3 from '../assets/images/blogImage3.jpg'
+import blogImage4 from '../assets/images/blogImage4.jpeg'
+import blogImage5 from '../assets/images/blogImage5.webp'
+import blogImage6 from '../assets/images/blogImage6.jpg'
+
+export const blogs = [
+  {
+    id: 1,
+    name: 'first blog',
+    image: blogImage1,
+    description: "some description"
+  },
+  {
+    id: 2,
+    name: 'second blog',
+    image: blogImage2,
+    description: "some description"
+  },
+  {
+    id: 3,
+    name: 'third blog',
+    image: blogImage3,
+    description: "some description"
+  },
+  {
+    id: 4,
+    name: 'fourth blog',
+    image: blogImage4,
+    description: "some description"
+  },
+  {
+    id: 5,
+    name: 'fifth blog',
+    image: blogImage5,
+    description: "some description"
+  },
+  {
+    id: 6,
+    name: 'sixth blog',
+    image: blogImage6,
+    description: "some description"
+  }  
+];
