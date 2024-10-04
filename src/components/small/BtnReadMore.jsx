7f08e8385703abcbd48c559992f2a03010c3634b
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-04 10:36:53 +0400

    create "read more" component

---
+++ src/components/small/BtnReadMore.jsx
@@ -0,0 +1,10 @@
+export default function BtnReadMore () {
+  return(
+    <a href="/blog" style={{
+      color: "black",
+      textDecoration: "underline overline",
+      }}>
+      Read more
+    </a>
+  );
+}
