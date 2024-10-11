d2db440bcc35fb882df02d9649a45a954c8fc3bb
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-11 11:29:28 +0400

    delete everrything in head except: description and title

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,14 +6,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
-        <meta name="theme-color" content="#000000" />
         <meta
           name="description"
           content="Web site created using create-react-app"
         />
-        <link rel="apple-touch-icon" href="%PUBLIC_URL%/logo192.png" />
-        <link rel="manifest" href="%PUBLIC_URL%/manifest.json" />
         <title>React App</title>
       </head>
       <body>
