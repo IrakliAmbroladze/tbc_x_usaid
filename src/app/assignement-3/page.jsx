d37f63b9345ce0733c08b023836f46bd0e09c09e
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-13 14:04:46 +0400

    just reorder code

--- src/app/assignement-3/page.jsx
+++ src/app/assignement-3/page.jsx
@@ -1,37 +1,34 @@
-
+export default function Assignement3(){
+  return <div className="container">{list}</div>
+}
 
 const obj = {
-  id: '10002',
-  name: 'Eco-Friendly Water Bottle',
-  description: 'Stay hydrated with our durable, eco-friendly water bottle.',
-  price: 14.99,
-  currency: 'USD',
-  imageURL: 'https://example.com/images/product-10002.jpg',
+id: '10002',
+name: 'Eco-Friendly Water Bottle',
+description: 'Stay hydrated with our durable, eco-friendly water bottle.',
+price: 14.99,
+currency: 'USD',
+imageURL: 'https://example.com/images/product-10002.jpg',
 };
 
 
 const listContent = Object.entries(obj).reduce((accumulator, [key, value], index) => {
-  accumulator.push(
-    <tr key={index}>
-      <td>{key}</td>
-      <td>{value}</td>
-      <td>{index}</td>
-    </tr>
-  );
-  return accumulator;
+accumulator.push(
+  <tr key={index}>
+    <td>{key}</td>
+    <td>{value}</td>
+    <td>{index}</td>
+  </tr>
+);
+return accumulator;
 }, [
-  <tr>
-    <th>Key</th>
-    <th>Value</th>
-    <th>Position</th>
+<tr>
+  <th>Key</th>
+  <th>Value</th>
+  <th>Position</th>
 </tr>
 ]);
 
 const list = <table>
-  {listContent}
-  </table>
-
-
-export default function Assignement3(){
-    return <>{list}</>
-}
+{listContent}
+</table>
