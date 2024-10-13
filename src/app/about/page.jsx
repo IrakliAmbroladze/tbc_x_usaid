a77be46f5f9c8f82440f768167fc999135c6671e
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-13 13:47:28 +0400

    create about route

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,10 +1,13 @@
 export default function About (){
   return (
+    <div className="container">
+
     <main className='about-main'>
       <b>About page</b>
       <section>
         some company selling some tables. თუ კონკრეტულად ამ პროექტის აღწერაზეა საუბარი, მეც არ ვიცი ეს საიტი რაზეა. ჩემი შავად სამუშაო სივრცეა ამ ეტაპზე
       </section>
     </main>
+    </div>
   )
 }
