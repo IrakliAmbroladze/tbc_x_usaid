93d3380d4f507665b3a76453a5cbd8d70a3e7a75
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-13 12:15:00 +0400

    delete [[...slug]] folder

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,13 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import Main from './components/Main';
-import Assignement3 from './components/Assignement3';
-import Profile from './components/Profile';
-import Blog from './components/Blog';
-import Contact from './components/Contact';
-import About from './components/About';
+import Header from '.../components/Header';
+import Footer from '.../components/Footer';
+import Main from '.../components/Main';
+import Assignement3 from '.../components/Assignement3';
+import Profile from '.../components/Profile';
+import Blog from '.../components/Blog';
+import Contact from '.../components/Contact';
+import About from '.../components/About';
 
 
 export default function App() {
