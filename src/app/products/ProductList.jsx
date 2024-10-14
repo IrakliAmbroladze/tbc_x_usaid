12479927177b1e6177acc4354b1441153832cd56
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-14 13:42:49 +0400

    give correct classNamrs to elements

--- src/app/products/ProductList.jsx
+++ src/app/products/ProductList.jsx
@@ -3,9 +3,9 @@ import "./ProductCard.css"
 
 export const ProductList = (props)=> {
   return (
-    <div className="main">
-      <h2>Item Shop</h2>
-      <div className="items">
+    <div className="container margin-top-20px margin-bottom-20px">
+      <h2 style={{textAlign: "center"}}>Item Shop</h2>
+      <div className="items margin-top-20px">
         {props.productList.map((product)=>(
           <ProductCard key={product.id} product={product} />
         ))}
