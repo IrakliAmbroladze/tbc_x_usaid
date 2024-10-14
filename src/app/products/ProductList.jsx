d7b663b308e351190438dffd53d7a975d71eab95
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-14 09:52:12 +0400

    change main element with div

--- src/app/products/ProductList.jsx
+++ src/app/products/ProductList.jsx
@@ -3,14 +3,14 @@ import "./ProductCard.css"
 
 export const ProductList = (props)=> {
   return (
-    <main className="main">
+    <div className="main">
       <h2>Item Shop</h2>
       <div className="items">
         {props.productList.map((product)=>(
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
-    </main>
+    </div>
   )
 }
 
