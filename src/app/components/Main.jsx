8db0de3ddba8a430dd89f9bc3f87f66c3d9c97e8
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-14 10:34:54 +0400

    add top margin css independently

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -2,7 +2,7 @@ import { ItemGrid } from './ItemGrid';
 
 export default function Main() {
   return (
-      <div className="container">
+      <div className="container margin-top-20px">
 
       <ItemGrid />
       </div>
