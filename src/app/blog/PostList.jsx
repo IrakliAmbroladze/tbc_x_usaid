572407b72fb1d5af2bbca27d5ccf53e45729e49f
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-14 16:21:32 +0400

    minor css update

--- src/app/blog/PostList.jsx
+++ src/app/blog/PostList.jsx
@@ -5,7 +5,7 @@ export const PostList = (props)=> {
   return (
     <div className="container margin-top-20px margin-bottom-20px">
       <h2 style={{textAlign: "center"}}>B L O G</h2>
-      <div className="items margin-top-20px">
+      <div className="postItems margin-top-20px">
         {props.postList.map((post)=>(
           <PostCard key={post.id} post={post} />
         ))}
@@ -13,22 +13,4 @@ export const PostList = (props)=> {
     </div>
   )
 }
-// import { blogs } from './Blog_data'
-// import BlogCart from './BlogCart'
-
-// export function BlogList() {
-
-//   return (
-//     <div className="container margin-top-20px">
-
-//     <div className='item_grid'>
-//       {blogs.map((product, key) =>
-//         <BlogCart key={key} data={product}/>
-        
-//       )}
-//     </div>
-//       </div>
-//   )
-// }
-
 
