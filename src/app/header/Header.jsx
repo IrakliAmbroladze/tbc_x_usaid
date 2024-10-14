939b0769e51b9dfa4d0ee0a32115adb5442ef85a
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-14 09:51:42 +0400

    create and add header css

--- src/app/header/Header.jsx
+++ src/app/header/Header.jsx
@@ -1,15 +1,16 @@
+import './header.css'
 import Link from "next/link"
 
 export default function Header() {
   return (
-    <header>
+    <header className="header">
       <nav>
-        <Link href="/">Home</Link> | 
-        <Link href="/assignement-3">Assignment 3</Link> | 
-        <Link href="/profile">Profile</Link> | 
-        <Link href="/products">Products</Link> | 
-        <Link href="/blog">BLOG</Link> | 
-        <Link href="/contact">Contact</Link> | 
+        <Link href="/">Home</Link>
+        <Link href="/assignement-3">Assignment 3</Link>
+        <Link href="/profile">Profile</Link>
+        <Link href="/products">Products</Link>
+        <Link href="/blog">BLOG</Link>
+        <Link href="/contact">Contact</Link>
         <Link href="/about">About</Link>
       </nav>
 
