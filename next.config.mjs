7a892bd9869b579baa1ef14728d6e1660139a546
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-16 11:30:50 +0400

    add dummyjson.com as sourse in config file

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
   output: 'export', // Outputs a Single-Page Application (SPA).
   distDir: './dist', // Changes the build output directory to ./dist/.
+  images: {
+    domains: ["cdn.dummyjson.com"],
+  }
 }
  
 export default nextConfig
