9cbd6b1ec3599a25536f5d2afa3ebf9db3828981
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-16 21:37:56 +0400

    add id

--- src/app/blog/PostCard.jsx
+++ src/app/blog/PostCard.jsx
@@ -4,10 +4,10 @@ import Link from "next/link"
 
 export const PostCard = (props) => {
   return (
-    <div className="postItem">
+    <div key={props.post.id} className="postItem">
           <h3 >{props.post.title}</h3>
         <div>
-          <Link href={`/blog/${props.post.id}`}>read more</Link>
+          <Link  href={`/blog/${props.post.id}`}>read more</Link>
         </div>
     </div>
   )
