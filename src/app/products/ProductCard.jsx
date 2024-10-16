24ffc2faec86ffcf237505411838781de0ca9b8b
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-16 13:19:34 +0400

    add key

--- src/app/products/ProductCard.jsx
+++ src/app/products/ProductCard.jsx
@@ -3,7 +3,7 @@ import Link from "next/link"
 
 export const ProductCard = (props) => {
   return (
-    <div className="item">
+    <div key={props.product.id} className="item">
         <div style={{
           height: 120
         }}>
