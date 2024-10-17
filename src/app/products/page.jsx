5b11ba3b80625edbc0a4d732941c9080950aa1a0
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-17 13:10:07 +0400

    add search input line

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -4,13 +4,20 @@ import { SortingButtons } from "../components/small/SortingButtons.jsx";
 
 export default async function Products() {
   const productList = await fetchProducts();
+
+  const header = <h2 style={{textAlign: "center"}} className="margin-top-20px">
+                    Item Shop
+                </h2>;
+
+  const searchAndSorting = <div style={{display: "flex", justifyContent: "space-between"}}>
+                              <input placeholder='search'></input>
+                              <SortingButtons />
+                            </div>
+
   return (
     <div className="container">
-      <h2 
-        style={{textAlign: "center"}}
-        className="container margin-top-20px"
-      >Item Shop</h2>
-      <SortingButtons />
+      {header}
+      {searchAndSorting}
       <ProductList productList={productList} />
     </div>
   );
