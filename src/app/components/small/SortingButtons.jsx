c3649e9ac7679fc9625e8222c0011ad81414288b
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-17 21:32:44 +0400

    create sorting buttons

--- src/app/components/small/SortingButtons.jsx
+++ src/app/components/small/SortingButtons.jsx
@@ -1,8 +1,24 @@
+"use client"
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+
 export function SortingButtons() {
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  
+  const handleSort = (order) => {
+    const params = new URLSearchParams(searchParams);
+    params.set('sortBy', "price");
+    params.set('order', order);
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div>
-      <button>Asc ↓</button>
-      <button>Desc ↑</button>
+      <b>price: </b> 
+      <button onClick={() =>handleSort("asc")}>Asc ↓</button>
+      <button onClick={() =>handleSort("desc")}>Desc ↑</button>
     </div>
   )
 }
+
