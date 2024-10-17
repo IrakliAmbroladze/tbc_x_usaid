c4ee1f67c1792d26b97abc94156bcc90a4cf173d
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-17 21:32:07 +0400

    take searchParams of sortBy and order

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -2,17 +2,22 @@ import { fetchProducts } from "./fetchProducts";
 import { ProductList } from "./ProductList";
 import { SortingButtons } from "../components/small/SortingButtons.jsx";
 
-export default async function Products() {
-  const productList = await fetchProducts();
+export default async function Products({searchParams}) {
+  const { sortBy, order } = searchParams;
+  const productList = await fetchProducts(sortBy, order);
 
-  const header = <h2 style={{textAlign: "center"}} className="margin-top-20px">
-                    Item Shop
-                </h2>;
+  const header = (
+    <h2 style={{textAlign: "center"}} className="margin-top-20px">
+      Item Shop
+    </h2>
+  );
 
-  const searchAndSorting = <div style={{display: "flex", justifyContent: "space-between"}}>
-                              <input placeholder='search'></input>
-                              <SortingButtons />
-                            </div>
+  const searchAndSorting = (
+    <div style={{display: "flex", justifyContent: "space-between"}}>
+      <input placeholder='search'></input>
+      <SortingButtons />
+    </div>
+  ); 
 
   return (
     <div className="container">
