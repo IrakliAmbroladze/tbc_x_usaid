b70f815ee837d60f2169bcdcf37a2f148e4cdf1b
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-18 00:15:15 +0400

    create Serch component

--- src/app/products/search.jsx
+++ src/app/products/search.jsx
@@ -1,9 +1,31 @@
-"use client"
+"use client";
+import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+
 
 export function Search() {
-  return (
-    // <div>search</div>
-    <input placeholder='search'></input>
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
 
+  const handleSearch = 
+  useDebouncedCallback((term) => {
+    const params = new URLSearchParams(searchParams);
+    if (term) {
+      params.set('query', term);
+    } else {
+      params.delete('query');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  return (
+    <input 
+    placeholder='search'
+    onChange={(e) => {
+      handleSearch(e.target.value);
+    }}
+    defaultValue={searchParams.get('query')?.toString()}
+    ></input>
   )
 }
