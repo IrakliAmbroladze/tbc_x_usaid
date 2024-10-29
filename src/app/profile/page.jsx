d9b9a25577777b19ef8210270b92493307ee716a
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-10-29 14:58:14 +0400

    feature (add): add context provider

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,6 +1,10 @@
+'use client'
 // import Header from "../components/header/Header";
 // import Footer from "../components/footer/Footer";
 import { UserProfile } from "./user-profile";
+import { ThemeContext, AuthContext } from './context';
+
+import {user} from './context'
 
 import "./index.css"
 
@@ -10,7 +14,10 @@ export default function Profile (){
         <div className="page-wrapper">
             {/* <Header /> */}
             <div className="default-layout">
+            <AuthContext.Provider value={user}>
+                {/* <div>${user}</div> */}
                 <UserProfile />
+                </AuthContext.Provider>
                 {/* <Footer /> */}
             </div>
         </div>
--- src/app/profile/user-profile.jsx
+++ src/app/profile/user-profile.jsx
@@ -1,9 +1,12 @@
 import {fetchUser} from "./fetchUser"
 import { AvatarName} from "./user-avatar-name"
 import { UserBackground } from "./user-background"
+import { useContext } from 'react';
+import {AuthContext} from './context'
 
 export async function UserProfile() {
-  const user = await fetchUser();
+  const user = useContext(AuthContext);
+  console.log(user)
   return (
     <div key={user.id} className="profile-card">
         
