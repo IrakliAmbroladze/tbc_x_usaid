b4b22bfedff2d6742e011b8a16559b238b7d709f
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-05 03:21:11 +0400

    feat(add): dark mode in html

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,10 +8,12 @@ export const metadata = {
 
 export default function RootLayout({children}) {
   return (
-    <html lang="en">
+    <html lang="en" class="dark">
       <UserProvider>
         <body className="page-wrapper">
-          {children}
+          <div class="bg-white dark:bg-black flex-1">
+            {children}
+          </div>
         </body>
       </UserProvider>
     </html>
