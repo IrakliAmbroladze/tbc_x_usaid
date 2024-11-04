536804291234e9bc45b9f873feac16c2a7454ee0
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-04 13:28:04 +0400

    component(add): create layout for authorized user view

---
+++ src/app/(authUserView)/layout.jsx
@@ -0,0 +1,24 @@
+import { getSession } from '@auth0/nextjs-auth0';
+import { redirect } from "next/navigation";
+import Header from '../header/Header.jsx'
+import Footer from '../footer/Footer.jsx'
+
+
+export default async function layout({children}) {
+  const session = await getSession();
+  const user = session?.user
+
+  if(!user){
+    redirect("/");
+  }
+  return (
+    <div className="page-wrapper">
+      <Header />
+      <div className="default-layout">
+        <main>{children}</main>
+        <Footer/>
+      </div>
+    </div>
+  )
+}
+
