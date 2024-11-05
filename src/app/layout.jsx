c2cfd92204ab1a26acdb4127396c88571f1033f6
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-05 11:41:26 +0400

    feat(update): dark mode colors for welcome page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -8,12 +8,10 @@ export const metadata = {
 
 export default function RootLayout({children}) {
   return (
-    <html lang="en" class="dark">
+    <html lang="en" className="dark">
       <UserProvider>
-        <body className="page-wrapper">
-          <div class="bg-white dark:bg-black flex-1">
+        <body className="page-wrapper bg-white dark:bg-stone-800">
             {children}
-          </div>
         </body>
       </UserProvider>
     </html>
