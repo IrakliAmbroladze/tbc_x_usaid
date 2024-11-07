81e58cfd45d18a3c728d7fdd46929640da1069db
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-07 14:39:48 +0400

    css(update): header opacity

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -3,7 +3,7 @@ import ThemeToggle from './ThemeToggle'
 
 export default function Header() {
   return (
-    <header className="text-center shadow-md h-12 fixed top-0 w-full z-50 flex justify-center items-center dark:text-white">
+    <header className="text-center shadow-md h-12 fixed top-0 w-full z-50 flex justify-center items-center dark:text-white bg-white dark:bg-stone-800 bg-opacity-[92%] dark:bg-opacity-[92%]">
       <nav className="flex justify-center flex-wrap gap-3 m-10">
   <Link href="/" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">Home</Link>
   <Link href="/assignment-3" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">Assignment 3</Link>
