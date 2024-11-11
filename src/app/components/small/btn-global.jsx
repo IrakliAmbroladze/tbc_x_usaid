b4009942ba4ddfb671b11510a27890afeb04fdd4
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-11 15:19:18 +0400

    rafact(component): add href as prop to button

--- src/app/components/small/btn-global.jsx
+++ src/app/components/small/btn-global.jsx
@@ -1,7 +1,10 @@
+import Link from 'next/link'
 import './btn-global.css'
 
-export default function BtnGlobal({children, height, width}) {
+export default function BtnGlobal({children, height, width, href}) {
   return (
+    <Link href={href}>
     <button className="btnGlobal" style={{height, width}}> {children} </button>
+    </Link>
   )
 }
