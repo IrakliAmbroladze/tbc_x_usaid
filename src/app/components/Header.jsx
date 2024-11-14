b4a577d8397d261724ef9193ae715b1ab2a60a1a
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-14 18:00:24 +0400

    feat(add): add dashboard page

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -13,6 +13,7 @@ export default function Header() {
     <header className="text-center shadow-md h-12 fixed top-0 w-full z-50 flex justify-center items-center dark:text-white bg-white dark:bg-stone-800 bg-opacity-[92%] dark:bg-opacity-[92%]">
       <nav className="flex justify-center flex-wrap gap-3 m-10">
         <Link href="/" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">{t('home')}</Link>
+        <Link href="/dashboard" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">{t("dashboard")}</Link>
         <Link href="/assignment-3" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">{t("assignment 3")}</Link>
         <Link href="/profile" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">{t('profile')}</Link>
         <Link href="/products" className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400">{t('products')}</Link>
