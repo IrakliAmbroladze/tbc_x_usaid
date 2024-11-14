edaf24e4c02e015bf9723bd5cd76884adb92c508
IrakliAmbroladze <ambroladzeirakli@gmail.com>
2024-11-15 01:18:00 +0400

    refact: update footer.

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,28 +1,20 @@
-import './footer.css'
 export default function Footer() {
   return (
-    <footer className='footer'>
+    <footer className='flex bg-gray-50 footer text-center justify-evenly'>
+      
       <div>
         <b> ჩვენ შესახებ</b>
-        <div className='footer-info'> მოსაყოლი ბევრია ... </div>
-        
-      </div>
-      <div>
-      <b> პროდუქტები</b>
-      <ul>
-        <li>ყავა</li>
-        <li>და</li>
-        <li>ლუდი</li>
-      </ul>
-      </div>
-      <div>
-      <b> კონტაქტი</b>
-      <div className='contact-info'>
-        <div>phone: +995 ... </div>
-        <div>e-mail: mail@mail.ge</div>
-         
+        <div> მოსაყოლი ბევრია ... </div>
       </div>
 
+      <div>
+        <b> პროდუქტები</b>
+        <div>ყავა და ლუდი</div>
+      </div>
+
+      <div>
+        <b> კონტაქტი</b>
+        <div>mail@mail.ge T: +995 ... </div>
       </div>
       
     </footer>
