5b56cab690b86d5209e7d89e35d6ba7da09c2d7b
Kadi033 <demetre33dev@gmail.com>
2024-11-20 01:57:23 +0400

    feat: fetch single product from API endpoint

--- src/app/[locale]/(authUserView)/products/[id]/fetchProduct.js
+++ src/app/[locale]/(authUserView)/products/[id]/fetchProduct.js
@@ -1,8 +1,14 @@
 export async function fetchProduct(id) {
+  const appUrl = process.env.AUTH0_BASE_URL;
   try {
-    const response = await fetch(`https://dummyjson.com/products/${id}`);
-    return response.json();
+    const response = await fetch(`${appUrl}/api/products/${id}`);
+    if (!response.ok) {
+      throw new Error("Failed to fetch product");
+    }
+
+    return await response.json();
   } catch (error) {
+    console.error("Error fetching product:", error);
     return {};
   }
 }
