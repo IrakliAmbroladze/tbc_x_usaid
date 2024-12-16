b25108dc0e18aa130518c30ee03f156a801838b8
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2024-12-16 14:20:26 +0400

    feat(pricing):Added info cards for free and basic plans

---
+++ src/app/[locale]/pricing/components/free-plan.tsx
@@ -0,0 +1,25 @@
+import BtnGlobal from "app/components/small/btn-global";
+import { useTranslations } from "next-intl";
+import React from "react";
+
+export default function FreePlan() {
+  const t = useTranslations("Pricing");
+
+  return (
+    <div className="bg-white dark:bg-gray-500 border dark:border-gray-800 rounded-lg shadow-lg p-6 text-center transform transition-transform duration-300 hover:scale-105">
+      <h2 className="text-2xl font-bold text-gray-800 dark:text-white">
+        {t("free")}
+      </h2>
+      <p className="mt-4 text-gray-600 dark:text-white">{t("freeBody")}</p>
+
+      <ul className="mt-6 space-y-4 text-gray-600 dark:text-white mb-6">
+        <li>✔ {t("freeOption1")}</li>
+        <li>✔ {t("freeOption2")}</li>
+      </ul>
+
+      <BtnGlobal href="" height="2rem" width="15rem">
+        {t("select")} {t("free")}
+      </BtnGlobal>
+    </div>
+  );
+}
--- src/app/[locale]/pricing/page.tsx
+++ src/app/[locale]/pricing/page.tsx
@@ -1,3 +1,23 @@
+import React from "react";
+import FreePlan from "./components/free-plan";
+import BasicPlan from "./components/basic-plan";
+import { useTranslations } from "next-intl";
+
 export default function Pricing() {
-  return <div>Pricing page</div>;
+  const t = useTranslations("Pricing");
+
+  return (
+    <>
+      <div className="flex items-center flex-col justify-center max-w-[1110px] mx-auto p-6">
+        <h1 className="text-4xl font-bold text-center dark:text-white mb-6">
+          {t("title")}
+        </h1>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          <FreePlan />
+          <BasicPlan />
+        </div>
+      </div>
+    </>
+  );
 }
