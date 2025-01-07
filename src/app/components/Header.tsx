218550a564d96075b50d189304872e56c61d7cdf
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-01-07 23:29:23 +0400

    refactor(header): add burger menu
    because of the need of burger menu, header.tsx component was transfered from server side to client side. Before being HeaderAuth component used server component and it is teporarily commented for not making error in header.tsx client component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,10 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import ThemeToggle from "./ThemeToggle";
 import { useTranslations } from "next-intl";
 import { Link } from "../../i18n/routing";
 import Language from "./language";
 import HeaderAuth from "./header-auth";
-
+import { FiMenu, FiX } from "react-icons/fi";
 type NavLinkProps = {
   href: string;
   children: React.ReactNode;
@@ -22,6 +23,8 @@ const NavLink: React.FC<NavLinkProps> = ({ href, children }) => (
 export default function Header(): JSX.Element {
   const t = useTranslations("Header");
 
+  const [menuOpen, setMenuOpen] = useState(false);
+
   const navLinks = [
     { href: "/", label: t("home") },
     { href: "/dashboard", label: t("dashboard") },
@@ -34,15 +37,31 @@ export default function Header(): JSX.Element {
   return (
     <header className="fixed top-0 z-50 flex h-12 w-full items-center justify-center bg-white bg-opacity-90 shadow-md dark:bg-stone-800 dark:bg-opacity-90 dark:text-white">
       <div className="container mx-auto flex justify-between items-center px-4">
-        <nav className="flex gap-3" aria-label="Main Navigation">
-          {navLinks.map(({ href, label }) => (
-            <NavLink key={href} href={href}>
-              {label}
-            </NavLink>
-          ))}
+        <button
+          className="block md:hidden text-2xl"
+          onClick={() => setMenuOpen(!menuOpen)}
+          aria-label="Toggle navigation menu"
+        >
+          {menuOpen ? <FiX /> : <FiMenu />}
+        </button>
+
+        <nav
+          className={`${
+            menuOpen ? "block" : "hidden"
+          } absolute top-12 left-0 w-full bg-white shadow-md dark:bg-stone-800 md:static md:block md:shadow-none`}
+          aria-label="Main Navigation"
+        >
+          <ul className="flex flex-col md:flex-row gap-3 p-4 md:p-0 md:gap-3">
+            {navLinks.map(({ href, label }) => (
+              <li key={href}>
+                <NavLink href={href}>{label}</NavLink>
+              </li>
+            ))}
+          </ul>
         </nav>
+
         <div className="flex items-center gap-3">
-          <HeaderAuth />
+          {/* <HeaderAuth /> */}
           <ThemeToggle />
           <Language />
         </div>
