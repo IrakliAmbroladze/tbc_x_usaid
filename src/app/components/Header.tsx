7f1dd60130cf79c5e8603a3fb9c1d938a5475410
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-01-07 21:44:37 +0400

    style(header): improve code style by creating const NavLink for className
    each link used to have the same classname and for DRY principle, const NavLink was creating to avoid repeating

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -5,48 +5,32 @@ import { Link } from "../../i18n/routing";
 import Language from "./language";
 import HeaderAuth from "./header-auth";
 
+type NavLinkProps = {
+  href: string;
+  children: React.ReactNode;
+};
+
+const NavLink: React.FC<NavLinkProps> = ({ href, children }) => (
+  <Link
+    href={href}
+    className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
+  >
+    {children}
+  </Link>
+);
+
 export default function Header(): JSX.Element {
   const t = useTranslations("Header");
 
   return (
     <header className="text-center shadow-md h-12 fixed top-0 w-full z-50 flex justify-center items-center dark:text-white bg-white dark:bg-stone-800 bg-opacity-[92%] dark:bg-opacity-[92%]">
       <nav className="flex justify-center flex-wrap gap-3 m-10">
-        <Link
-          href="/"
-          className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
-        >
-          {t("home")}
-        </Link>
-        <Link
-          href="/dashboard"
-          className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
-        >
-          {t("dashboard")}
-        </Link>
-        <Link
-          href="/profile"
-          className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
-        >
-          {t("profile")}
-        </Link>
-        <Link
-          href="/products"
-          className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
-        >
-          {t("products")}
-        </Link>
-        <Link
-          href="/blog"
-          className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
-        >
-          {t("blog")}
-        </Link>
-        <Link
-          href="/pricing"
-          className="dark:text-white no-underline transition duration-300 p-2 hover:text-gray-400"
-        >
-          {t("pricing")}
-        </Link>
+        <NavLink href="/">{t("home")}</NavLink>
+        <NavLink href="/dashboard">{t("dashboard")}</NavLink>
+        <NavLink href="/profile">{t("profile")}</NavLink>
+        <NavLink href="/products">{t("products")}</NavLink>
+        <NavLink href="/blog">{t("blog")}</NavLink>
+        <NavLink href="/pricing">{t("pricing")}</NavLink>
         <HeaderAuth />
       </nav>
       <ThemeToggle />
