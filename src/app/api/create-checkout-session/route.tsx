71262909719f46cf5ac691ff7950d23b6f3da243
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-01-14 15:55:37 +0400

    feat(subscription api): create stripe custumer and connect to supabase user
    When the use (signed in via supabase subscripes, stripe user is created and in supabase table called user-profiles, supabase user id and stripe user id is connected as a foreign key. these gives us a posiibility to identify user who has subscribed and who have not, after which we can allow or deny access on specific routes e.x. dashboard for subscribed users and for not subscribers

--- src/app/api/create-checkout-session/route.tsx
+++ src/app/api/create-checkout-session/route.tsx
@@ -10,6 +10,10 @@ export async function POST(req: Request): Promise<Response> {
     data: { user },
   } = await supabase.auth.getUser();
 
+  const customer = await stripe.customers.create({
+    email: user?.email,
+  });
+
   try {
     const {
       items,
@@ -28,11 +32,18 @@ export async function POST(req: Request): Promise<Response> {
       payment_method_types: ["card"],
       line_items: transformedItems,
       mode: "subscription",
-      customer_email: user?.email,
-
+      customer: customer.id,
       success_url: `${process.env.NEXT_PUBLIC_URL}/${langCookie}/pricing/success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${process.env.NEXT_PUBLIC_URL}/${langCookie}/pricing`,
     });
+
+    const { error } = await supabase
+      .from("user_profiles")
+      .upsert({ id: user?.id, stripe_customer_id: customer.id });
+
+    if (error) {
+      throw new Error("Failed to update user profile with Stripe customer ID");
+    }
 
     return NextResponse.json({ id: session.id });
   } catch (error: any) {
