76ceaaeb975d63dc6affd6371d579018b246ed31
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-01-21 15:23:27 +0400

    fix(pricing): use public url for subscribe

--- src/app/api/create-checkout-session/route.tsx
+++ src/app/api/create-checkout-session/route.tsx
@@ -31,17 +31,23 @@ export async function POST(req: Request): Promise<Response> {
 
     const myCookies = cookies();
     const langCookie = myCookies.get("NEXT_LOCALE")?.value || "en";
-    const baseUrl = process.env.NEXT_PUBLIC_VERCEL_URL
-      ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
-      : "http://localhost:3000";
+    const getBaseUrl = () => {
+      if (process.env.NEXT_PUBLIC_URL) {
+        return process.env.NEXT_PUBLIC_URL;
+      } else if (process.env.VERCEL_URL) {
+        return `https://${process.env.VERCEL_URL}`;
+      } else {
+        return "http://localhost:3000";
+      }
+    };
 
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
       line_items: transformedItems,
       mode: "subscription",
       customer: customer.id,
-      success_url: `${baseUrl}/${langCookie}/pricing/success?session_id={CHECKOUT_SESSION_ID}`,
-      cancel_url: `${baseUrl}/${langCookie}/pricing`,
+      success_url: `${getBaseUrl()}/${langCookie}/pricing/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${getBaseUrl()}/${langCookie}/pricing`,
     });
 
     const { error } = await supabase
